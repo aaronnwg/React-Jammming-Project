298b5421a98637c8bfaff56d8b75cb95273e86ca
Aaron <kesslerawkessler@gmail.com>
2022-11-25 10:14:00 -0700

    created remove track method

--- jammming/src/Components/App/App.js
+++ jammming/src/Components/App/App.js
@@ -26,6 +26,12 @@ class App extends React.Component {
     this.setState({playlistTracks: tracks})
   }
 
+  removeTrack(track){
+    let tracks = this.state.playlistTracks;
+    tracks = tracks.filter(currentTrack => currentTrack.id !== track.id);
+    this.setState({ playlistTracks: tracks });
+  }
+
   render(){
     return (
     <div>
