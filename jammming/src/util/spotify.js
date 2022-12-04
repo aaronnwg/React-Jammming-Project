6b70841b7f385f63db7503aa18155760af7b0904
Aaron <kesslerawkessler@gmail.com>
2022-12-04 09:41:19 -0700

    set expiration for accessToken. clear parameters

--- jammming/src/util/spotify.js
+++ jammming/src/util/spotify.js
@@ -1,5 +1,23 @@
-const Spotify = {
+let accessToken;
 
+const Spotify = {
+    getAccessToken() {
+        if(accessToken) {
+            return accessToken;
+        }
+        // checks for accessToken Match
+        const accesssTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+        const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+        if (accessTokenMatch && expiresInMatch) {
+            accessToken = accessTokenMatch[1];
+            const expiresIn = Number(expiresInMatch[1]);
+            //this clears the parametes, allowing us to acess a new token when it expires.
+            window.setTimeout(() => accessToken = '', expiresIn * 1000);
+            window.history.pushState('Access Token', null, '/');
+            return accessToken;
+        }
+    }
 }
 
 export default Spotify;
