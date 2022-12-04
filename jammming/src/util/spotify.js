61e50eac99eb16d676ee5f35cc77e0f7adf48669
Aaron <kesslerawkessler@gmail.com>
2022-12-04 09:56:40 -0700

    set application’s client ID and redirect URI.

--- jammming/src/util/spotify.js
+++ jammming/src/util/spotify.js
@@ -1,3 +1,5 @@
+const clientId = '143accff119c4de88e82b37743d7da5c';//api client ID
+const redirectUri = 'http://localhost:3000/';
 let accessToken;
 
 const Spotify = {
@@ -16,6 +18,9 @@ const Spotify = {
             window.setTimeout(() => accessToken = '', expiresIn * 1000);
             window.history.pushState('Access Token', null, '/');
             return accessToken;
+        } else {
+            const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
+            window.location = accessUrl;
         }
     }
 }
